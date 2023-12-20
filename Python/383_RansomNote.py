47fa539229c41752d2825abbd555cc2e0ce3ee26
marc-caramello <83841077+marc-caramello@users.noreply.github.com>
2023-12-20 05:13:06 -0500

    Delete Python directory

--- Python/383_RansomNote.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for currentLetter in ransomNote:
-            if(currentLetter in magazine):
-                magazine = ''.join(magazine.split(currentLetter, 1))
-            else:
-                return False
-        return True
